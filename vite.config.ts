97beefb4f9ada575c989ff98b68ed5b1f1bb592e
hmdnikoo <hmd.nikoo@gmail.com>
2025-11-17 16:22:28 +0100

    appled changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,28 +2,22 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import https from 'https'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
-  const isDev = mode === 'development'
 
   return {
     plugins: [vue(), vueDevTools()],
     server: {
+      host: true,
+      port: 5173,
       proxy: {
         '/api': {
-          target: env.VITE_API_URL,
+          target: env.VITE_API_URL,   // MUST be "http://localhost:8080"
           changeOrigin: true,
-          ...(isDev && {
-            secure: false,
-            agent: new https.Agent({
-              rejectUnauthorized: false,
-            }),
-          }),
-          rewrite: (path) => path.replace(/^\/api/, '/api'),
-        },
-      },
+          secure: false,
+        }
+      }
     },
     resolve: {
       alias: {
